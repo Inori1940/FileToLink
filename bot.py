41ac74dcd53b3577414fd11ea5b2dfdc26097f56
Inori1940 <lazydog295@gmail.com>
2024-11-16 18:14:00 +0630

    Testone

--- bot.py
+++ bot.py
@@ -3,23 +3,22 @@ import time
 import pyshorteners
 import os
 
-bot = telebot.TeleBot(token=os.getenv('TG_BOT_TOKEN'))
+bot = telebot.TeleBot(token=os.getenv('7388672963:AAEcxCeYpHO50zw03-H-YS28RVX'))
 
 def short(url):
     return pyshorteners.Shortener().tinyurl.short(url)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, 'Heya! I am a File To Link Bot created by TeLe TiPs.Send me any file (Video, Audio, Photo, Document)👇🏻')
+    bot.reply_to(message, ' o yo yo. 148-3 to the 3 to the 6 to the 9, representing the ABQ, what up, biatch?!')
 
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
-    bot.reply_to(message, 'Send me any type of a file & I will send you the shorten link of it')    
+    bot.reply_to(message, 'All digital products and in game currency in one place expect girlfriend 😁')    
 
-@bot.message_handler(content_types=['photo', 'video', 'audio', 'document'])
-def file_sent(message):
-    try:
-        bot.send_message(message.chat.id, short(bot.get_file_url(message.document.file_id)))
+@bot.message_handler(content_types=['In game currency', 'Digital products', 'Social app services', 'Premium Services'])
+def file_sent(message
+        bot.send_message(message.MLBBDiamond, shot.get_file_url(message.document.file_id)))
     except AttributeError:
         try:
             bot.send_message(message.chat.id, short(bot.get_file_url(message.photo[0].file_id)))
